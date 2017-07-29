3944e659b5d71feeacb5e28f65648775b1afbf5a
Ajoy Oommen <ajoyoommen@gmail.com>
2017-07-29 16:08:54 +0530

    Add MEDIA_URL and MEDIA_ROOT

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'home/ubuntu/healthRecord_web/'
+STATIC_ROOT = 'home/ubuntu/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'home/ubuntu/media/'
